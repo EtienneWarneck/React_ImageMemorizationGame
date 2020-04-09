736ef527d3c0b732c7545e5869e7fc524eb00fb6
Etienne Warneck <etienne.warneck@gmail.com>
2020-04-09 15:16:22 -0400

    adding screenshots

---
+++ build/precache-manifest.7c755ddd78f7f2d3dab41dede34d0619.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12f2035e94751444818b0526f9d84e10",
+    "url": "/React_ImageMemorizationGame/index.html"
+  },
+  {
+    "revision": "69480b89a595e0aedf5a",
+    "url": "/React_ImageMemorizationGame/static/css/main.b164276d.chunk.css"
+  },
+  {
+    "revision": "9ed1161593a757502c7f",
+    "url": "/React_ImageMemorizationGame/static/js/2.eb1f1eda.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/React_ImageMemorizationGame/static/js/2.eb1f1eda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "69480b89a595e0aedf5a",
+    "url": "/React_ImageMemorizationGame/static/js/main.35de79c7.chunk.js"
+  },
+  {
+    "revision": "2eb0eeedb8e4ad427df8",
+    "url": "/React_ImageMemorizationGame/static/js/runtime-main.6f3f1f9c.js"
+  }
+]);
